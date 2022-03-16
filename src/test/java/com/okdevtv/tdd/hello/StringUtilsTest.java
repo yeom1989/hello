4e288748cee4fd966083d8689b6aee7df88a2894
yeom1989 <junghoon.yeom89@gmail.com>
2022-03-16 11:15:06 +0900

    💚 mvn compiler #3

--- src/test/java/com/okdevtv/tdd/hello/StringUtilsTest.java
+++ src/test/java/com/okdevtv/tdd/hello/StringUtilsTest.java
@@ -12,7 +12,7 @@ public class StringUtilsTest {
 		assertEquals("1,000", StringUtils.comma(1000));
 		assertEquals("10,000", StringUtils.comma(10000));
 		assertEquals("100,000", StringUtils.comma(100000));
-		assertEquals("1,200,000", StringUtils.comma(12000000));
+		assertEquals("1,200,000", StringUtils.comma(1200000));
 	}
 
 	
